f94f8a1d40630e336fd0c270a8077a36a4a18257
Billy Bastardi <billy@yext.com>
2014-04-21 02:26:47 -0400

    minor logic and comment cleanup

--- deps/deps.go
+++ deps/deps.go
@@ -41,45 +41,46 @@ func main() {
 	rootNode := Node{
 		Path: "/src/",
 	}
+
 	// Iterate through the packages imports
 	for i := range imports {
+		// Store the full path of our import
+		path := imports[i]
 
-		// For each import, create a node
-		base := imports[i]
-
-		currentNode := nodes[base]
-		for base != "" {
-			// If the full path exits, start at it
-			if currentNode != nil {
+		// For each directory in the path, create a node and link it
+		// to it's parent and children
+		for path != "" {
+			// Create our starter node
+			if currentNode == nil {
+				currentNode = &Node{
+					Path: path,
+				}
+			} else {
 				// Before creating a new parent node,
 				// check to see if one exists
-				if nodes[base] == nil {
+				if nodes[path] == nil {
 					currentNode.setParent(&Node{
-						Path: base,
+						Path: path,
 					})
 					// Change the current node to the newly created item
 					currentNode = currentNode.Parent
 				} else {
-					currentNode.setParent(nodes[base])
+					currentNode.setParent(nodes[path])
 					currentNode = nil
 					break
-				}
-			} else {
-				currentNode = &Node{
-					Path: base,
 				}
 			}
 
 			// Constant time lookup to all of our nodes
 			// based on their full path string
-			nodes[base] = currentNode
+			nodes[path] = currentNode
 
 			// Keep popping off the tip of the path
-			base, _ = filepath.Split(base)
+			path, _ = filepath.Split(path)
 
 			// Trailing slash in file path causes issues, remove it
-			if strings.HasSuffix(base, "/") {
-				base = base[:len(base)-1]
+			if strings.HasSuffix(path, "/") {
+				path = path[:len(path)-1]
 			}
 		}
 
