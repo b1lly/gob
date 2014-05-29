19c3a9938b56eeaa1807551dff96fb6149d8c238
ttacon <ttacon@gmail.com>
2014-05-29 11:39:25 -0400

    Add forgotten file, whoops :P

---
+++ signal.go
@@ -0,0 +1,27 @@
+package gob
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+// this function allows us to tell gob to restart the process it is running
+func registerSignalHandlers(g *Gob) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT)
+	go func() {
+		for _ = range c {
+			// waiting for CTRL-C
+			select {
+			case <-time.After(time.Second):
+				// TODO(ttacon): ensure we kill child process
+				g.Print("\r[gob] exiting...")
+				os.Exit(0)
+			case <-c:
+				g.restartApp()
+			}
+		}
+	}()
+}
